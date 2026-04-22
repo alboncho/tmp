577a761623906fd1f537c44833623dd536f90f64
alboncho <144751035+alboncho@users.noreply.github.com>
2026-04-21 21:55:46 -0400

    Add existence check logs for box_procesador
    Added console logs for existence check of box_procesador.

--- procesador.js
+++ procesador.js
@@ -1,7 +1,8 @@
 let box_procesador =  document.getQuerySelector('.box-procesador');
 
 if (box_procesador) {
-   box_procesador.innerHTML = 'hola';
+   console.log("existe");
 } else {
    console.log("No existe el contenedor');
+   let x = documente.createElement('div');
 }
